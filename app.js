3239a9d3cea0a89a5284b5524f8fcd8390a37b9b
durga598 <durga598@gmail.com>
2016-12-17 17:37:15 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,69 @@
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate', 'toastr', 'ui.router', 'satellizer', 'ngMaterial'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+
+    /**
+     * Helper auth functions
+     */
+    var skipIfLoggedIn = ['$q', '$auth', function($q, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.reject();
+      } else {
+        deferred.resolve();
+      }
+      return deferred.promise;
+    }];
+
+    var loginRequired = ['$q', '$location', '$auth', function($q, $location, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/login');
+      }
+      return deferred.promise;
+    }];
+
+    /**
+     * App routes
+     */
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        controller: 'LoginCtrl',
+        templateUrl: 'partials/login.html',
+        resolve:{
+          skipIfLoggedIn:skipIfLoggedIn
+        }
+      })
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/home.html',
+        // controller: 'LoginCtrl',
+        resolve: {
+          loginRequired: loginRequired
+        }
+      })
+      // .state('signup', {
+      //   url: '/signup',
+      //   templateUrl: 'partials/signup.html',
+      //   controller: 'SignupCtrl',
+      //   resolve: {
+      //     skipIfLoggedIn: skipIfLoggedIn
+      //   }
+      // })
+      // .state('logout', {
+      //   url: '/logout',
+      //   template: null,
+      //   controller: 'LogoutCtrl'
+      // })
+      // .state('profile', {
+      //   url: '/profile',
+      //   templateUrl: 'partials/profile.html',
+      //   controller: 'ProfileCtrl',
+      //   resolve: {
+      //     loginRequired: loginRequired
+      //   }
+      // });
+    $urlRouterProvider.otherwise('/');
+});
